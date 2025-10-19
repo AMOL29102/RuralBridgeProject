30987c97402d09971834a0843ae792ae4de4b359
Amol <amolpatilamp2910@gmail.com>
2025-10-19 13:02:35 +0530

    Frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL:  "https://rural-bridge-project.vercel.app" || "http://localhost:5000" });
 
 export const setToken = (token) => {
     if(token) {
