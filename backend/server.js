652208e692b71f7b05213da97af053d1643b1c98
Amol <amolpatilamp2910@gmail.com>
2025-10-19 12:59:47 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api',(req,res)=>res.send("API is running..."));
+
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
